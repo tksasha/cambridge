fd5121f0bc405dbcceb1ae65bac01b1e3aaf2eae
Alexander Tkachuk <tksasha@yandex.ru>
2010-01-26 16:39:32 +0300

    change :get to :put

--- test/functional/answers_controller_test.rb
+++ test/functional/answers_controller_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 class AnswersControllerTest < ActionController::TestCase
   def setup 
     @answer = answers(:answer_one)
+    @answer.update_attribute :text, 'Bla-bla, bla-bla-bla, bla-bla'
   end
 
-  should_route :get, '/answers/1/check/text_of_answer', :controller => :answers, :action => :check, :id => 1, :text => 'text_of_answer'
+  should_route :put, '/answers/1/check', :controller => :answers, :action => :check, :id => 1
 
   test 'get check' do
-    get :check, :id => @answer.id, :text => 'fu'
+    put :check, :id => @answer.id, :text => 'Bla-bla, bla-bla-bla, bla-bla'
     
     assert_response :success
     assert assigns(:answer)
